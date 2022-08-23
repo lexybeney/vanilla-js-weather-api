4fc5def402cedc8565b243b46462cb9a582cba2b
Your Name <you@example.com>
2022-08-23 17:59:04 +0100

    added files from other folder to new repo

---
+++ htmlCreator.js
@@ -0,0 +1,58 @@
+export const updateInterface = (location, data, latitude, longitude) => {
+  // current day weather
+  let html = `<h1>${location}</h1>
+            <h2>Today</h2>
+             <p><strong>${Math.round(
+               data.daily[0].temp.max
+             )} &#8451;</strong> ${Math.round(data.daily[0].temp.min)} &#8451;`;
+
+  let map = `<iframe
+      width="500"
+      height="400"
+      frameborder="0"
+      src="https://www.bing.com/maps/embed?h=400&w=500&cp=${latitude}~${longitude}&lvl=12.371139810340651&typ=d&sty=r&src=SHELL&FORM=MBEDV8"
+      scrolling="no"
+    >
+    </iframe>`;
+
+  document.getElementById("location").innerHTML = html;
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("map").innerHTML = map;
+
+  // next 6 days forecast
+  let forecast = data.daily;
+  let htmlForecast = "";
+
+  const sevenDays = forecast.splice(1, 7);
+  for (const day of sevenDays) {
+    htmlForecast += generateForecast(day);
+    document.getElementById("forecastDays").innerHTML = htmlForecast;
+  }
+};
+
+export const generateForecast = (day) => {
+  const forecastDay = new Date(day.dt * 1000);
+  const dateEnd = dateOrdinal(forecastDay.getDate());
+
+  function dateOrdinal(d) {
+    return d == 31 || d == 21 || d == 1
+      ? "st"
+      : d == 22 || d == 2
+      ? "nd"
+      : d == 23 || d == 3
+      ? "rd"
+      : "th";
+  }
+
+  const formattedDate = forecastDay.toLocaleDateString("en-GB", {
+    day: "numeric",
+    weekday: "short",
+  });
+
+  return `<div id=${
+    day.dt
+  }><h3>${formattedDate}${dateEnd}</h3><p><strong>${Math.round(
+    day.temp.max
+  )} &#8451;</strong> ${Math.round(day.temp.min)} &#8451;</p>
+            </div>`;
+};
---
+++ weather.js
@@ -0,0 +1,51 @@
+// import modules
+import { updateInterface } from "./htmlCreator.js";
+
+// DOM references
+const searchLocation = document.getElementById("searchLocation");
+
+// Get user location coords with browser geolocation API
+navigator.geolocation.getCurrentPosition(
+  (location) => {
+    getWeatherData(location.coords.latitude, location.coords.longitude);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+// Use location coords to get weather information from third-party API
+async function getWeatherData(latitude, longitude) {
+  const weatherData = await axios.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&units=metric&appid=b6877a79366a3f016b75f846e2c979a1`
+  );
+  getLocationFromCoords(latitude, longitude, weatherData);
+}
+
+// Use coords to get town from third-party API and update interface with this data
+async function getLocationFromCoords(latitude, longitude, weatherData) {
+  const location = await axios.get(
+    `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=5&appid=b6877a79366a3f016b75f846e2c979a1`
+  );
+  updateInterface(location.data[0].name, weatherData.data, latitude, longitude);
+}
+
+// User input location sent to third-party API to get coords
+async function getLocationFromPlace(userInput) {
+  try {
+    const coords = await axios.get(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=b6877a79366a3f016b75f846e2c979a1`
+    );
+    getWeatherData(coords.data[0].lat, coords.data[0].lon);
+  } catch (error) {
+    let html = `<p>This doesn't look quite right, please try again.</p>`;
+    document.getElementById("error").innerHTML = html;
+  }
+}
+
+searchLocation.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputData = new FormData(searchLocation);
+  const inputEntry = Object.fromEntries(inputData);
+  getLocationFromPlace(inputEntry.search);
+});
